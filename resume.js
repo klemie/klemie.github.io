bd83fe91aba879b32384b6e240cc8b7548726215
klemie <lemieuxkristopher@gmail.com>
2021-03-09 23:57:40 -0800

    add onto responsive webdesign

--- resume.js
+++ resume.js
@@ -2,3 +2,10 @@ function toggleCard(id){
     const card = document.getElementById(id);
     card.classList.toggle("ion-hide");
 }
+//responsive 
+
+const x = window.matchMedia("(min-width: 620px)");
+const av = document.getElementById("avcon");
+if(x.matches){
+    av.classList.toggle("center");
+}
